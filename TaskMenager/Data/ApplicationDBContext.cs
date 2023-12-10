948f990c215bab4b5921802a0359b122a521d289
Mateusz <matekudr@gmail.com>
2023-12-10 12:20:53 +0100

    Add db context and  add a service to program.cs

---
+++ TaskMenager/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TaskMenager.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        
+        
+            public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+            {
+
+            }
+        
+    }
+}
