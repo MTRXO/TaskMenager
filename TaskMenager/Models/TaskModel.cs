14042b31d34da1428b79b8289557671ef8b6492c
Mateusz <matekudr@gmail.com>
2023-12-10 12:12:10 +0100

    Write model

---
+++ TaskMenager/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskMenager.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TaskTitle { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        public DateTime CreationTime { get; set; } = DateTime.Now;  
+    }
+}
