18c1ee6d64f11ba5adb9b55f3106d058feeab837
Mateusz <matekudr@gmail.com>
2023-12-11 00:27:50 +0100

    Create a tab ActiveTasksController and view and geting data from database

---
+++ TaskMenager/Controllers/ActiveTasksController.cs
@@ -0,0 +1,22 @@
+﻿using TaskMenager.Models;
+using Microsoft.AspNetCore.Mvc;
+using TaskMenager.Data;
+
+namespace TaskMenager.Controllers
+{
+    public class ActiveTasksController : Controller
+    {
+        private ApplicationDBContext _db;
+
+        public ActiveTasksController(ApplicationDBContext db)
+        {
+                _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<TaskModel> objTask = _db.Tasks.ToList(); 
+            return View(objTask);
+        }
+    }
+}
