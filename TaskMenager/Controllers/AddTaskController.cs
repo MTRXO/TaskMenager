bcf79093ccea9cedd495c57b013da344c9640442
Mateusz <matekudr@gmail.com>
2023-12-11 22:04:24 +0100

    Write a code to sending data to DB and create TaskReady View

--- TaskMenager/Controllers/AddTaskController.cs
+++ TaskMenager/Controllers/AddTaskController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskMenager.Models;
+using TaskMenager.Data;
 
 namespace TaskMenager.Controllers
 {
     public class AddTaskController : Controller
     {
+        private ApplicationDBContext _db;
+
+        public AddTaskController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View(); 
         }
+
+        [HttpPost]
+
+        public IActionResult Create(TaskModel obj)
+        {
+            _db.Tasks.Add(obj);
+            _db.SaveChanges();
+            return View("TaskReady");  
+        }
+
     }
 }
