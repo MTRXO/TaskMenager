7cd5e637b30b26c0a4ea8dc7c1398549e165bf28
Mateusz <matekudr@gmail.com>
2023-12-11 20:55:31 +0100

    create AddTaskController and View

---
+++ TaskMenager/Controllers/AddTaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMenager.Controllers
+{
+    public class AddTaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
