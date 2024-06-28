71f22d79e022c9e00286934575e992c111c6ced5
Mateusz <matekudr@gmail.com>
2024-06-28 20:00:33 +0200

    Add IDbOperations interface

---
+++ DataAccess/Repository/DbOperations/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.DbOperations
+{
+    public interface IDbOperations<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Updater(T entity);
+        void Delete(T entity);
+        void RemoveRange (IEnumerable<T> entity);
+        
+    }
+}
